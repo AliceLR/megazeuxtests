33f8d4b4aaa6baff3b1579ecd401c6cc37781466
AliceLR <petrifiedrowan@gmail.com>
2021-11-24 21:09:12 -0700

    modutil: add IT pattern loading/dumping.

--- src/it_load.cpp
+++ src/it_load.cpp
@@ -235,6 +235,144 @@ struct IT_sample
   uint32_t largest_block;
 };
 
+struct IT_event
+{
+  enum
+  {
+    NOTE            = (1 << 0),
+    INSTRUMENT      = (1 << 1),
+    VOLUME          = (1 << 2),
+    EFFECT          = (1 << 3),
+    LAST_NOTE       = (1 << 4),
+    LAST_INSTRUMENT = (1 << 5),
+    LAST_VOLUME     = (1 << 6),
+    LAST_EFFECT     = (1 << 7),
+
+    CHANNEL         = 0x3f,
+    READ_MASK       = (1 << 7),
+
+    NO_VOLUME       = 0,
+    SET_VOLUME      = 1,
+    SET_PAN         = 2,
+    FINE_VOLUME_UP  = 3,
+    FINE_VOLUME_DN  = 4,
+    VOLUME_UP       = 5,
+    VOLUME_DN       = 6,
+    PORTA_UP        = 7,
+    PORTA_DN        = 8,
+    TONEPORTA       = 9,
+    VIBRATO         = 10,
+    VOLUME_INVALID  = 11,
+    NUM_VOLUME_FX   = 12
+  };
+
+  uint8_t note = 0;
+  uint8_t instrument = 0;
+  uint8_t volume_effect = NO_VOLUME;
+  uint8_t volume_param = 0;
+  uint8_t effect = 0;
+  uint8_t param = 0;
+
+  IT_event() {}
+
+  void set_volume(uint8_t volume)
+  {
+    if(volume <= 64)
+    {
+      volume_effect = SET_VOLUME;
+      volume_param  = volume;
+    }
+    else
+
+    if(volume >= 65 && volume <= 74)
+    {
+      volume_effect = FINE_VOLUME_UP;
+      volume_param  = volume - 65;
+    }
+    else
+
+    if(volume >= 75 && volume <= 84)
+    {
+      volume_effect = FINE_VOLUME_DN;
+      volume_param  = volume - 75;
+    }
+    else
+
+    if(volume >= 85 && volume <= 94)
+    {
+      volume_effect = VOLUME_UP;
+      volume_param  = volume - 85;
+    }
+    else
+
+    if(volume >= 95 && volume <= 104)
+    {
+      volume_effect = VOLUME_DN;
+      volume_param  = volume - 95;
+    }
+    else
+
+    if(volume >= 105 && volume <= 114)
+    {
+      volume_effect = PORTA_UP;
+      volume_param  = volume - 105;
+    }
+    else
+
+    if(volume >= 115 && volume <= 124)
+    {
+      volume_effect = PORTA_DN;
+      volume_param  = volume - 115;
+    }
+    else
+
+    if(volume >= 128 && volume <= 192)
+    {
+      volume_effect = SET_PAN;
+      volume_param  = volume - 128;
+    }
+    else
+
+    if(volume >= 193 && volume <= 202)
+    {
+      volume_effect = TONEPORTA;
+      volume_param  = volume - 193;
+    }
+    else
+
+    if(volume >= 203 && volume <= 212)
+    {
+      volume_effect = VIBRATO;
+      volume_param  = volume - 203;
+    }
+    else
+    {
+      volume_effect = VOLUME_INVALID;
+      volume_param  = volume;
+    }
+  }
+};
+
+struct IT_pattern
+{
+  IT_event *events = nullptr;
+  uint16_t raw_size = 0;
+  uint16_t num_rows = 0;
+  uint8_t  num_channels = 0;
+
+  IT_pattern() {}
+
+  ~IT_pattern()
+  {
+    delete[] events;
+  }
+
+  void allocate()
+  {
+    events = new IT_event[num_rows * num_channels]{};
+  }
+};
+
 struct IT_header
 {
   /*  00 */ char     magic[4]; /* IMPM */
@@ -268,16 +406,20 @@ struct IT_data
   IT_header     header;
   IT_sample     *samples = nullptr;
   IT_instrument *instruments = nullptr;
+  IT_pattern    *patterns = nullptr;
   uint8_t       *orders = nullptr;
   uint32_t      *instrument_offsets = nullptr;
   uint32_t      *sample_offsets = nullptr;
   uint32_t      *pattern_offsets = nullptr;
   bool          uses[NUM_FEATURES];
 
+  size_t        num_channels;
+
   ~IT_data()
   {
     delete[] samples;
     delete[] instruments;
+    delete[] patterns;
     delete[] orders;
     delete[] instrument_offsets;
     delete[] sample_offsets;
@@ -674,6 +816,143 @@ static modutil::error IT_read_old_instrument(FILE *fp, IT_instrument &ins)
 }
 
 /**
+ * Scan an IT pattern. This is required to determine the number of stored channels.
+ */
+static modutil::error IT_scan_pattern(IT_pattern &p, const uint8_t *stream)
+{
+  p.num_channels = 0;
+
+  uint8_t mask[64]{};
+  for(size_t row = 0, i = 0; row < p.num_rows && i < p.raw_size;)
+  {
+    size_t flags = stream[i++];
+    if(flags == 0)
+    {
+      row++;
+      continue;
+    }
+
+    size_t channel = (flags - 1) & IT_event::CHANNEL;
+    if(channel >= p.num_channels)
+      p.num_channels = channel + 1;
+
+    if(flags & IT_event::READ_MASK)
+    {
+      if(p.raw_size - i < 1)
+        return modutil::INVALID;
+
+      mask[channel] = stream[i++];
+    }
+
+    size_t skip = 0;
+
+    if(mask[channel] & IT_event::NOTE)
+      skip += 1;
+    if(mask[channel] & IT_event::INSTRUMENT)
+      skip += 1;
+    if(mask[channel] & IT_event::VOLUME)
+      skip += 1;
+    if(mask[channel] & IT_event::EFFECT)
+      skip += 2;
+
+    if(p.raw_size - i < skip)
+      return modutil::INVALID;
+
+    i += skip;
+  }
+  return modutil::SUCCESS;
+}
+
+/**
+ * Read an IT pattern.
+ */
+static modutil::error IT_read_pattern(IT_pattern &p, const uint8_t *stream)
+{
+  if(p.num_rows < 1 || p.num_channels < 1)
+    return modutil::SUCCESS;
+
+  p.allocate();
+
+  uint8_t mask[64]{};
+  uint8_t last_note = 0;
+  uint8_t last_instrument = 0;
+  uint8_t last_volume = 0;
+  uint8_t last_effect = 0;
+  uint8_t last_param = 0;
+
+  for(size_t row = 0, i = 0; row < p.num_rows && i < p.raw_size;)
+  {
+    size_t flags = stream[i++];
+    if(flags == 0)
+    {
+      row++;
+      continue;
+    }
+
+    size_t channel = (flags - 1) & IT_event::CHANNEL;
+    if(channel >= p.num_channels)
+      return modutil::INVALID;
+
+    if(flags & IT_event::READ_MASK)
+    {
+      if(p.raw_size - i < 1)
+        return modutil::INVALID;
+
+      mask[channel] = stream[i++];
+    }
+
+    IT_event &ev = p.events[row * p.num_channels + channel];
+    if(mask[channel] & IT_event::NOTE)
+    {
+      if(p.raw_size - i < 1)
+        return modutil::INVALID;
+
+      ev.note = last_note = stream[i++];
+    }
+
+    if(mask[channel] & IT_event::INSTRUMENT)
+    {
+      if(p.raw_size - i < 1)
+        return modutil::INVALID;
+
+      ev.instrument = last_instrument = stream[i++];
+    }
+
+    if(mask[channel] & IT_event::VOLUME)
+    {
+      if(p.raw_size - i < 1)
+        return modutil::INVALID;
+
+      last_volume = stream[i++];
+      ev.set_volume(last_volume);
+    }
+
+    if(mask[channel] & IT_event::EFFECT)
+    {
+      if(p.raw_size - i < 2)
+        return modutil::INVALID;
+
+      ev.effect = last_effect = stream[i++];
+      ev.param  = last_param  = stream[i++];
+    }
+
+    if(mask[channel] & IT_event::LAST_NOTE)
+      ev.note = last_note;
+    if(mask[channel] & IT_event::LAST_INSTRUMENT)
+      ev.instrument = last_instrument;
+    if(mask[channel] & IT_event::LAST_VOLUME)
+      ev.set_volume(last_volume);
+    if(mask[channel] & IT_event::LAST_EFFECT)
+    {
+      ev.effect = last_effect;
+      ev.param  = last_param;
+    }
+  }
+  return modutil::SUCCESS;
+}
+
+
+/**
  * Read an IT file.
  */
 static modutil::error IT_read(FILE *fp)
@@ -686,6 +965,8 @@ static modutil::error IT_read(FILE *fp)
 
   if(strncmp(h.magic, "IMPM", 4))
     return modutil::FORMAT_ERROR;
+
+  num_its++;
 
   if(!fread(h.name, 26, 1, fp))
     return modutil::READ_ERROR;
@@ -861,7 +1142,51 @@ static modutil::error IT_read(FILE *fp)
     }
   }
 
-  /* TODO load patterns. */
+  /* Load patterns. */
+  if(h.num_patterns)
+  {
+    m.patterns = new IT_pattern[h.num_patterns];
+
+    std::vector<uint8_t> patbuf(65536);
+
+    for(size_t i = 0; i < h.num_patterns; i++)
+    {
+      if(m.pattern_offsets[i] == 0)
+        continue;
+
+      if(fseek(fp, m.pattern_offsets[i], SEEK_SET))
+        return modutil::SEEK_ERROR;
+
+      IT_pattern &p = m.patterns[i];
+
+      /* Header. */
+      p.raw_size = fget_u16le(fp);
+      p.num_rows = fget_u16le(fp);
+      fget_u32le(fp);
+
+      if(!p.raw_size || !p.num_rows)
+        continue;
+
+      if(!fread(patbuf.data(), p.raw_size, 1, fp))
+      {
+        format::warning("read error at pattern %zu", i);
+        continue;
+      }
+
+      modutil::error ret = IT_scan_pattern(p, patbuf.data());
+      if(ret)
+      {
+        format::warning("error scanning pattern %zu", i);
+        continue;
+      }
+      ret = IT_read_pattern(p, patbuf.data());
+      if(ret)
+      {
+        format::warning("error loading pattern %zu", i);
+        continue;
+      }
+    }
+  }
 
   format::line("Name",     "%s", h.name);
   format::line("Type",     "IT %x (T:%x %03x)", h.format_version, (h.tracker_version >> 12), (h.tracker_version & 0xFFF));
@@ -1030,10 +1355,62 @@ static modutil::error IT_read(FILE *fp)
 
   if(Config.dump_patterns)
   {
-    // FIXME
+    format::line();
+    format::orders("Orders", m.orders, h.num_orders);
+
+    if(!Config.dump_pattern_rows)
+      format::line();
+
+    for(size_t i = 0; i < h.num_patterns; i++)
+    {
+      IT_pattern &p = m.patterns[i];
+
+      /* lol */
+      static constexpr char chrs[IT_event::NUM_VOLUME_FX] =
+      {
+        ' ', 'v', 'p', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', '?'
+      };
+
+      struct volumeIT
+      {
+        uint8_t volume_effect;
+        uint8_t volume_param;
+        static constexpr int width() { return 4; }
+        bool can_print() const { return volume_effect != IT_event::NO_VOLUME; }
+        void print() const { if(can_print()) fprintf(stderr, " %c%02x", chrs[volume_effect], volume_param); else format::spaces(width()); }
+      };
+
+      using EVENT = format::event<format::note, format::sample, volumeIT, format::effectIT>;
+      format::pattern<EVENT> pattern(i, p.num_channels, p.num_rows, p.raw_size);
+
+      if(!Config.dump_pattern_rows)
+      {
+        pattern.summary();
+        continue;
+      }
+      if(!p.events)
+      {
+        pattern.print();
+        continue;
+      }
+
+      IT_event *current = p.events;
+      for(size_t row = 0; row < p.num_rows; row++)
+      {
+        for(size_t track = 0; track < p.num_channels; track++, current++)
+        {
+          format::note     a{ current->note };
+          format::sample   b{ current->instrument };
+          volumeIT         c{ current->volume_effect, current->volume_param };
+          format::effectIT d{ current->effect, current->param };
+
+          pattern.insert(EVENT(a, b, c, d));
+        }
+      }
+      pattern.print();
+    }
   }
 
-  num_its++;
   return modutil::SUCCESS;
 }
 
